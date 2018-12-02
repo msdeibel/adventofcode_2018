23fba71a8263713d0fd4ab3746ed159b8f1e897d
msdeibel <git2018@markus-deibel.de>
2018-12-02 08:49:08 +0100

    Door 2

---
+++ adventofcode_2018/120202.fs
@@ -0,0 +1,289 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Linq.Expressions
+open System.Linq.Expressions
+
+[<EntryPoint>]
+let main argv =
+
+    let input = "evsialkqydurohxqpwbcugtjmh
+evsialkqydurohxzssbcngtjmv
+fvlialkqydurohxzpwbcngujmf
+nvsialkqydorohxzpwpcngtjmf
+evsialjqydnrohxypwbcngtjmf
+vvsialyqxdurohxzpwbcngtjmf
+yvsialksydurowxzpwbcngtjmf
+evsillkqydurbhxzpmbcngtjmf
+ivsialkqyxurshxzpwbcngtjmf
+ejsiagkqyduhohxzpwbcngtjmf
+evsialkqldurohxzpcbcngtjmi
+evsialkqydurohxzpsbyngtkmf
+ersialkeydurohxzpwbcngtpmf
+evsialuqzdkrohxzpwbcngtjmf
+evswulkpydurohxzpwbcngtjmf
+evsialkqyiurohxzpwucngttmf
+evtialkqydurphxzywbcngtjmf
+evsialkzyiurohxzpwbcxgtjmf
+evsiaykqydurohxzpwbcggtjuf
+evxqalkqydurohmzpwbcngtjmf
+eisralkqydurohxzpdbcngtjmf
+evsfalkqydurohxzpwbangtjwf
+evbialkqydurohxzawbcngtjmg
+evsialkqydrrohxrpcbcngtjmf
+evsialkqycurohxzpvbcngtjkf
+evsialkqsdudohxzpwbcnotjmf
+evsiackqydurohxzpmbsngtjmf
+evsialmqykurohxzpwbfngtjmf
+evsialsqydurohxzpwucngtjxf
+tvsialkqyeurohxzpwbcrgtjmf
+zvsialkqydbrohxzpwbcnltjmf
+evsmbskqydurohxzpwbcngtjmf
+evsialkqydurohxzpwbcngpgmt
+evsialkqydurlyezpwbcngtjmf
+evoialkqyturohxzpwbcnjtjmf
+evsialkqydurohxspkfcngtjmf
+evsiaikqydurohxjpwbcngtjmd
+evsialkyydurohxzvwbcngtjmc
+svsialkqyduhohxzpwbhngtjmf
+eysillkqydurohxzhwbcngtjmf
+evsialkqyduetaxzpwbcngtjmf
+evsialkqxdurshxzpwbcngtjmb
+evsiadkqydwrovxzpwbcngtjmf
+evsialkqydurokxzpwbcngjjef
+evskalkqymurohxzpybcngtjmf
+cvsialkqydurohxzpwbcnbtjma
+evsialkqydurohxzawhcngtjuf
+evsiahkqfduroixzpwbcngtjmf
+evsivlkqyduroqxzpwbctgtjmf
+evsiarkqyduroixzywbcngtjmf
+evspalkqydurohxzpwlcngxjmf
+eesialkqydurohxzpalcngtjmf
+gvsualkqydurohxzpwbmngtjmf
+evsialkqydurlhxzpwbcngsjmq
+evsialhqydfrohxopwbcngtjmf
+evzialkqydsrohxzpwbcngtjmw
+evbpalkqydurbhxzpwbcngtjmf
+mvsialkqydurohxzpwbcnghjmr
+evsialkqsdurohxzpkbcngtjxf
+ejkialktydurohxzpwbcngtjmf
+evsialkqyauoohxzpwbqngtjmf
+evsiklkyyduroqxzpwbcngtjmf
+evgialkqydurohxzpwocngthmf
+ebsialkqydcrohxzpwbcngtbmf
+evsialkqysurohxzpwfingtjmf
+evsialkqddurmhxzpwbnngtjmf
+evsialkqydurohxoiwwcngtjmf
+evsialkqydurohpzkzbcngtjmf
+vvsealkqydurorxzpwbcngtjmf
+evsialkqyduroqxzpwlungtjmf
+eviialkqiyurohxzpwbcngtjmf
+evzsalkqyaurohxzpwbcngtjmf
+exsialkqydurohfzpwbwngtjmf
+evsialkqyduruhxkpwbcnytjmf
+essiatkqydurohxzpwbxngtjmf
+evsialkqyduroamzpwbcngtjcf
+wvsialkqyduruhxzpwbcnxtjmf
+evsialkqydurohxgpwbcngtjeh
+evsialfqxdurohxzpwbcngtomf
+evsialkqyourghxzpwbcngtbmf
+evsoaokqydurohxzpwbcngtamf
+evsialpqydurohxzpwccxgtjmf
+evsialkqzdurxhxgpwbcngtjmf
+ezsialkqmdurohxzpwbcngtjmi
+cvsialjeydurohxzpwbcngtjmf
+evsialkqydurocxupwbcvgtjmf
+evscalkqydtrohxzpebcngtjmf
+evjialkqyduiohxzpabcngtjmf
+evsialjqyduruhxzppbcngtjmf
+evsialkqydurfhxzpwbcuqtjmf
+evsialkqyiurohizpwucngttmf
+evsialiqydurrhxzpwbcngdjmf
+evbialkqywurohxzpwhcngtjmf
+evsialkqyduloyxzpwbqngtjmf
+evsialxqyduzohxzpwbqngtjmf
+vvsialkqydurohxzpwbcnqpjmf
+evsialksydurohxzcwbmngtjmf
+pvsialkqydurohxzpwucngtjvf
+evsialkqydurohmkpwbcngtfmf
+mvsialkqydurphyzpwbcngtjmf
+evsialkqydyrohxzhwbcnitjmf
+evsialokydurozxzpwbcngtjmf
+evsialkqyduroexfcwbcngtjmf
+evsiavkqydurohxzpwbcnmtjme
+evsiawkqydurohxzpwbcngojjf
+evsialkaydurohxzpwfcngtjff
+evsialkaydurohxzpwbcngtjpb
+gvsialkqyburorxzpwbcngtjmf
+evszalkqydurphxzpwocngtjmf
+evsualkqyduropxzpwbcngejmf
+evsitlkqydurshxzpwbcngtkmf
+evbixlkqydrrohxzpwbcngtjmf
+elsialkqydprohxzpwbcngtrmf
+evsialkqydurohbzpwbcggtjmc
+evtoalqqydurohxzpwbcngtjmf
+evsralhqydurohxzowbcngtjmf
+evsialkhydurohxzlsbcngtjmf
+evsialkqydurohxvpwbcnuujmf
+evsialkqydurocxzuwbcngtjmi
+evsialkqndyrokxzpwbcngtjmf
+evsialkqydurywfzpwbcngtjmf
+evsialkqydurohxzwwbcngthms
+eqsiahkqydurohxzpwbyngtjmf
+evsdalkqydurohxzpwbcnjkjmf
+evsialkqyddrohplpwbcngtjmf
+evshalkqydurohxzpfxcngtjmf
+evvialkqydurohxapwbcngtjmh
+evsialkqyduvohxzpwbcnnvjmf
+evsiblkqedurohxzpwbkngtjmf
+evsvalkqfdutohxzpwbcngtjmf
+evsialjqydurohxzpwbcnctjsf
+evsialkxywurohxdpwbcngtjmf
+evsiagkqydurohxzpwzcjgtjmf
+ebsialkqydurohxzpxfcngtjmf
+evsialkqysfrohxzpwbcngtjlf
+evvialkqyqurwhxzpwbcngtjmf
+evxialkqydurohxzpwgcnrtjmf
+vvsillkqydurohxzpwbcvgtjmf
+evsiwlkqyduoohxzpwbcngtjxf
+evsialkqypurohezpwbcngtjwf
+evbialkqydurohxipwbcnftjmf
+evsiakkqyduyohxzpwbcngtjmu
+evsialkqydurohzzpwxqngtjmf
+evsialkqykurkhxzpwocngtjmf
+dvriplkqydurohxzpwbcngtjmf
+evsialkqgdurohxzpwbmnctjmf
+evsialkqyuurohxzpwtcngtjmj
+wvsialkqydurohxzpwbchgejmf
+eusimlsqydurohxzpwbcngtjmf
+evsialkqydqrohxzhwbcngtjmh
+wvswalkqydurohxzpwbcngjjmf
+evsialkqyourohxzkwbcngttmf
+evaialkqydurohxzbubcngtjmf
+evfialkqydueohxzpwbclgtjmf
+evrialkqydurohxzpwbcnctjmh
+evsiaojqydxrohxzpwbcngtjmf
+evsualkqywuxohxzpwbcngtjmf
+evsialkdydrzohxzpwbcngtjmf
+evlialkqyfurohxzpwbcnotjmf
+epsialkqydujohxzpwbcngtjif
+evsialkqyaucohxgpwbcngtjmf
+lvsialaqydurohxzpwbcngtjzf
+evsialkgydurohezpwbcngtjmo
+lvsialkqydurosxwpwbcngtjmf
+evsiaekqyqurohxzpvbcngtjmf
+evsiapkqydirohxzpwbzngtjmf
+zvsixlkwydurohxzpwbcngtjmf
+evaialkqyduoohxzpwbcngtjkf
+evsialcqedurohxzpwbcngtjmc
+evjialkgydurohxzpwbwngtjmf
+evsialkqcdurohxzpwbcpgojmf
+evsialkqkdurohxzlwbcngtrmf
+eosiylkzydurohxzpwbcngtjmf
+evsialkqydurohhzpwscnmtjmf
+evsiallqydurobxzpwbxngtjmf
+evsialkqydurohwztwhcngtjmf
+evsiallqydurohxzpwbcygjjmf
+evsiabkqywurohxzpwbcngtjmy
+evsiackqydzrohxznwbcngtjmf
+evsiazkqzdurooxzpwbcngtjmf
+evsialcqydurghxzpwbcngtjmc
+yvsiaxkqydurohxzpwbcxgtjmf
+evsiylkqgdhrohxzpwbcngtjmf
+lvsialkqydurohxgcwbcngtjmf
+evsiglkqydurohxzpwbvngzjmf
+evsialkqyvurohxzpwbcngtjnz
+evsialkgydueohxzpwbcpgtjmf
+cvsiavkqyddrohxzpwbcngtjmf
+evsialklyrurohxzpwbcngtjff
+eisialkqyduwohxzpwbcngcjmf
+evsialkqydrrihwzpwbcngtjmf
+easialkqydurohxzpwbcnltrmf
+evsialfqydurohxzpybcnytjmf
+eqsialkqycurohxzywbcngtjmf
+evsitlkqmdurohxzpwbcngtjmx
+evsiclsqyduroixzpwbcngtjmf
+elsialrqydurohxzpwmcngtjmf
+evsiapkqodurohxzpwbcogtjmf
+evstalkeydurohxzpibcngtjmf
+evsihlkqyqurohxzpwblngtjmf
+euszalkqydurohxipwbcngtjmf
+ezsialksydurohxzpwbcngfjmf
+eisialkdydurohxzpwbcngtumf
+evsirlkaydprohxzpwbcngtjmf
+evsiklkqydnrohxzpwbcngtjmu
+evsialkqydnuohxzpwbcngtjmu
+eksialkqydurohxztwfcngtjmf
+evlialkqedurohxzpwbhngtjmf
+evqialkqydurohxzpubcngtjpf
+evsialkwydurohwzpwbcnmtjmf
+evsiaokqcdurohxzpwbcngtjcf
+evsialkkyfurohxzpvbcngtjmf
+evsialkqyduromxzpwqcngtimf
+evsialkqydumohxzpwbcnmtjsf
+evsialddydurehxzpwbcngtjmf
+evsialkqydurohxzpobcnptjmk
+evsiagkqydurohhzpwbcxgtjmf
+evsfalkqydurohszpwbangtjmf
+evgialkzyduqohxzpwbcngtjmf
+evaialkqzdurohxzpwbcngtjmo
+evsialkqyqurohxjpwbcnntjmf
+evsialkjydybohxzpwbcngtjmf
+evskalgqydurohxzrwbcngtjmf
+evsialkqydurohxzpjbcymtjmf
+evsialkqqdurohxzpybcngtjyf
+evsialkqydqrbhxzpwbcngtjmj
+evssalaqrdurohxzpwbcngtjmf
+mvsialkfydurohxzpwbcngtjmk
+evsialkqwdurohxzpwgcngtjdf
+evqkalkqydurohxzpwbcngajmf
+evbialkqydurohxzpibcngejmf
+evszalkqydurbhxzpwbcngtjsf
+evsialkqydurohxepwbcngtjjo
+evsialkqcdubmhxzpwbcngtjmf
+evsiarkqyduroaxzpwbcngtjmp
+evsiakkqyduzohczpwbcngtjmf
+evtualkqydurofxzpwbcngtjmf
+ejsialkqvdurohzzpwbcngtjmf
+evsialkqydurohczpwbcngqvmf
+svsianfqydurohxzpwbcngtjmf
+evsialiqydurohxzpwbcngzqmf
+ejsialhqydurohxzpwjcngtjmf
+evpialkqydurohxzpwbcnbtjff
+evsialkuyvurohxzpwbcngtjkf
+eqsialkqydurohxzpwbcnwtcmf
+evsiatkqydkrohxzpwkcngtjmf
+evsialkqydurohxzpebciytjmf
+evsialkqydrrohxzpwtcngtfmf
+evsialkqjducohxzpwycngtjmf
+evsialkqydurohxzpwicnxtjnf"
+
+    let codes = input.Split "\r\n"
+
+    let mutable oneDiffCodes = Set.empty
+
+    let codeDifferences (c1:string) (c2:string) = 
+        let mutable diffs = 0
+        if c1 <> c2 then
+            for i in 0 .. (c1.Length - 1) do
+                if c1.[i] <> c2.[i] then
+                    diffs <- diffs + 1
+                else 
+                    ()
+
+        diffs
+
+    for code in codes do
+        for i in 0 .. codes.Length - 1 do
+            let differences = codeDifferences code codes.[i]
+            if differences = 1 then
+                ignore(oneDiffCodes <- oneDiffCodes.Add(code))
+                ignore(oneDiffCodes <- oneDiffCodes.Add(codes.[i]))
+            else 
+                ()
+            
+
+    Console.WriteLine("Done." + oneDiffCodes.ToString())
+    
+    ignore(Console.ReadLine())
+    0
